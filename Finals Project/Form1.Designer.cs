6ed4f4bcd1ba6bce436cf35fa6b585a614bcab7e
laragrace30 <laragrace.riconalla.20@usjr.edu.ph>
2024-07-17 21:43:35 +0800

    Added the swap language

--- Finals Project/Form1.Designer.cs
+++ Finals Project/Form1.Designer.cs
@@ -28,21 +28,203 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.bunifuGradientPanel1 = new Bunifu.Framework.UI.BunifuGradientPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.close = new System.Windows.Forms.PictureBox();
+            this.firstLanguage = new Bunifu.Framework.UI.BunifuDropdown();
+            this.bunifuElipse1 = new Bunifu.Framework.UI.BunifuElipse(this.components);
+            this.bunifuElipse2 = new Bunifu.Framework.UI.BunifuElipse(this.components);
+            this.secondLanguage = new Bunifu.Framework.UI.BunifuDropdown();
+            this.bunifuElipse3 = new Bunifu.Framework.UI.BunifuElipse(this.components);
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.bunifuElipse4 = new Bunifu.Framework.UI.BunifuElipse(this.components);
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.bunifuElipse5 = new Bunifu.Framework.UI.BunifuElipse(this.components);
+            this.translate = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.bunifuGradientPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.close)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // bunifuGradientPanel1
+            // 
+            this.bunifuGradientPanel1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("bunifuGradientPanel1.BackgroundImage")));
+            this.bunifuGradientPanel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.bunifuGradientPanel1.Controls.Add(this.pictureBox1);
+            this.bunifuGradientPanel1.Controls.Add(this.translate);
+            this.bunifuGradientPanel1.Controls.Add(this.textBox2);
+            this.bunifuGradientPanel1.Controls.Add(this.textBox1);
+            this.bunifuGradientPanel1.Controls.Add(this.secondLanguage);
+            this.bunifuGradientPanel1.Controls.Add(this.firstLanguage);
+            this.bunifuGradientPanel1.Controls.Add(this.close);
+            this.bunifuGradientPanel1.Controls.Add(this.label1);
+            this.bunifuGradientPanel1.GradientBottomLeft = System.Drawing.Color.FromArgb(((int)(((byte)(69)))), ((int)(((byte)(104)))), ((int)(((byte)(220)))));
+            this.bunifuGradientPanel1.GradientBottomRight = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(78)))), ((int)(((byte)(214)))));
+            this.bunifuGradientPanel1.GradientTopLeft = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(78)))), ((int)(((byte)(214)))));
+            this.bunifuGradientPanel1.GradientTopRight = System.Drawing.Color.FromArgb(((int)(((byte)(131)))), ((int)(((byte)(77)))), ((int)(((byte)(155)))));
+            this.bunifuGradientPanel1.Location = new System.Drawing.Point(-1, 0);
+            this.bunifuGradientPanel1.Name = "bunifuGradientPanel1";
+            this.bunifuGradientPanel1.Quality = 10;
+            this.bunifuGradientPanel1.Size = new System.Drawing.Size(886, 420);
+            this.bunifuGradientPanel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Heebo", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(380, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(140, 39);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Translate";
+            // 
+            // close
+            // 
+            this.close.BackColor = System.Drawing.Color.Transparent;
+            this.close.Image = ((System.Drawing.Image)(resources.GetObject("close.Image")));
+            this.close.Location = new System.Drawing.Point(842, 12);
+            this.close.Name = "close";
+            this.close.Size = new System.Drawing.Size(24, 24);
+            this.close.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.close.TabIndex = 1;
+            this.close.TabStop = false;
+            this.close.MouseClick += new System.Windows.Forms.MouseEventHandler(this.close_MouseClick);
+            // 
+            // firstLanguage
+            // 
+            this.firstLanguage.BackColor = System.Drawing.Color.Transparent;
+            this.firstLanguage.BorderRadius = 3;
+            this.firstLanguage.DisabledColor = System.Drawing.Color.Gray;
+            this.firstLanguage.ForeColor = System.Drawing.Color.White;
+            this.firstLanguage.Items = new string[0];
+            this.firstLanguage.Location = new System.Drawing.Point(124, 75);
+            this.firstLanguage.Name = "firstLanguage";
+            this.firstLanguage.NomalColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.firstLanguage.onHoverColor = System.Drawing.Color.FromArgb(((int)(((byte)(36)))), ((int)(((byte)(129)))), ((int)(((byte)(77)))));
+            this.firstLanguage.selectedIndex = -1;
+            this.firstLanguage.Size = new System.Drawing.Size(193, 35);
+            this.firstLanguage.TabIndex = 2;
+            // 
+            // bunifuElipse1
+            // 
+            this.bunifuElipse1.ElipseRadius = 20;
+            this.bunifuElipse1.TargetControl = this;
+            // 
+            // bunifuElipse2
+            // 
+            this.bunifuElipse2.ElipseRadius = 30;
+            this.bunifuElipse2.TargetControl = this.firstLanguage;
+            // 
+            // secondLanguage
+            // 
+            this.secondLanguage.BackColor = System.Drawing.Color.Transparent;
+            this.secondLanguage.BorderRadius = 3;
+            this.secondLanguage.DisabledColor = System.Drawing.Color.Gray;
+            this.secondLanguage.ForeColor = System.Drawing.Color.White;
+            this.secondLanguage.Items = new string[0];
+            this.secondLanguage.Location = new System.Drawing.Point(571, 75);
+            this.secondLanguage.Name = "secondLanguage";
+            this.secondLanguage.NomalColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.secondLanguage.onHoverColor = System.Drawing.Color.FromArgb(((int)(((byte)(36)))), ((int)(((byte)(129)))), ((int)(((byte)(77)))));
+            this.secondLanguage.selectedIndex = -1;
+            this.secondLanguage.Size = new System.Drawing.Size(193, 35);
+            this.secondLanguage.TabIndex = 3;
+            // 
+            // bunifuElipse3
+            // 
+            this.bunifuElipse3.ElipseRadius = 30;
+            this.bunifuElipse3.TargetControl = this.secondLanguage;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Heebo", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(42, 156);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(350, 175);
+            this.textBox1.TabIndex = 4;
+            // 
+            // bunifuElipse4
+            // 
+            this.bunifuElipse4.ElipseRadius = 25;
+            this.bunifuElipse4.TargetControl = this.textBox1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Heebo", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(493, 156);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(350, 175);
+            this.textBox2.TabIndex = 5;
+            // 
+            // bunifuElipse5
+            // 
+            this.bunifuElipse5.ElipseRadius = 25;
+            this.bunifuElipse5.TargetControl = this.textBox2;
+            // 
+            // translate
+            // 
+            this.translate.BackColor = System.Drawing.Color.White;
+            this.translate.Location = new System.Drawing.Point(364, 364);
+            this.translate.Name = "translate";
+            this.translate.Size = new System.Drawing.Size(156, 27);
+            this.translate.TabIndex = 6;
+            this.translate.Text = "TRANSLATE";
+            this.translate.UseVisualStyleBackColor = false;
+            this.translate.MouseClick += new System.Windows.Forms.MouseEventHandler(this.translate_MouseClick);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(426, 230);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(36, 29);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseClick);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(886, 420);
+            this.Controls.Add(this.bunifuGradientPanel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.bunifuGradientPanel1.ResumeLayout(false);
+            this.bunifuGradientPanel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.close)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private Bunifu.Framework.UI.BunifuGradientPanel bunifuGradientPanel1;
+        private System.Windows.Forms.Label label1;
+        private Bunifu.Framework.UI.BunifuDropdown firstLanguage;
+        private System.Windows.Forms.PictureBox close;
+        private Bunifu.Framework.UI.BunifuElipse bunifuElipse1;
+        private Bunifu.Framework.UI.BunifuElipse bunifuElipse2;
+        private Bunifu.Framework.UI.BunifuDropdown secondLanguage;
+        private Bunifu.Framework.UI.BunifuElipse bunifuElipse3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button translate;
+        private System.Windows.Forms.TextBox textBox2;
+        private Bunifu.Framework.UI.BunifuElipse bunifuElipse4;
+        private Bunifu.Framework.UI.BunifuElipse bunifuElipse5;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
--- Finals Project/Form1.cs
+++ Finals Project/Form1.cs
@@ -1,9 +1,11 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,9 +14,192 @@ namespace Finals_Project
 {
     public partial class Form1 : Form
     {
+        private HttpClient httpClient;
+        private Dictionary<string, string> languageDictionary;
+        private const string DetectLanguageApiKey = "bb3e95c93e9dde3314f34b34a8a13c60";
         public Form1()
         {
             InitializeComponent();
+            httpClient = new HttpClient();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            PopulateLanguages();
+
+            firstLanguage.selectedIndex = 0;
+            secondLanguage.selectedIndex = 1;
+        }
+
+        private void PopulateLanguages()
+        {
+            languageDictionary = new Dictionary<string, string>
+            {
+                { "English", "en" },
+                { "Spanish", "es" },
+                { "French", "fr" },
+                { "Dutch", "nl" },
+                { "Chinese", "zh" },
+                { "German", "de" },
+                { "Italian", "it" },
+                { "Portuguese", "pt" },
+                { "Russian", "ru" },
+                { "Arabic", "ar" },
+                { "Hindi", "hi" },
+                { "Bengali", "bn" },
+                {"Turkmen","tk" },
+                {"Zulu","zu" }
+            };
+
+            foreach (var language in languageDictionary.Keys)
+            {
+                firstLanguage.AddItem(language);
+                secondLanguage.AddItem(language);
+            }
+        }
+
+        private async Task<string> TranslateAsync(string text, string firstlang, string seclang)
+        {
+            string url = $"http://api.mymemory.translated.net/get?q={Uri.EscapeDataString(text)}&langpair={firstlang}|{seclang}";
+
+            try
+            {
+                HttpResponseMessage response = await httpClient.GetAsync(url);
+                response.EnsureSuccessStatusCode();
+
+                string responseJson = await response.Content.ReadAsStringAsync();
+                var translationResult = JsonConvert.DeserializeObject<TranslationResponse>(responseJson);
+
+                if (translationResult.ResponseStatus == 200 && translationResult.ResponseData != null)
+                {
+                    if (string.IsNullOrEmpty(translationResult.ResponseData.TranslatedText))
+                    {
+                        throw new Exception("Translation not provided by the API.");
+                    }
+
+                    return translationResult.TranslatedText;
+                }
+                else
+                {
+                    throw new Exception("Translation request failed.");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Translation failed. Error: \r\n" + ex.Message);
+                return string.Empty;
+            }
+        }
+
+        private async Task<string> DetectLanguageAsync(string text)
+        {
+            string url = $"https://ws.detectlanguage.com/0.2/detect?q={Uri.EscapeDataString(text)}&key={DetectLanguageApiKey}";
+
+            HttpResponseMessage response = await httpClient.GetAsync(url);
+            response.EnsureSuccessStatusCode();
+
+            string responseJson = await response.Content.ReadAsStringAsync();
+            var detectionResult = JsonConvert.DeserializeObject<LanguageDetectionResponse>(responseJson);
+
+            if (detectionResult.Data.Detections.Count > 0)
+            {
+                return detectionResult.Data.Detections[0].Language;
+            }
+            else
+            {
+                throw new Exception("Language detection failed.");
+            }
+        }
+
+        private void close_MouseClick(object sender, MouseEventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private async void translate_MouseClick(object sender, MouseEventArgs e)
+        {
+            try
+            {
+                string detectedLanguage = await DetectLanguageAsync(textBox1.Text);
+                string secLang = languageDictionary[secondLanguage.selectedValue.ToString()];
+
+                if (!languageDictionary.ContainsValue(detectedLanguage))
+                {
+                    MessageBox.Show("Detected language is not supported.");
+                    return;
+                }
+
+                if (detectedLanguage == secLang)
+                {
+                    MessageBox.Show("Please select a different target language.");
+                    return;
+                }
+
+                string translatedText = await TranslateAsync(textBox1.Text, detectedLanguage, secLang);
+                textBox2.Text = translatedText;
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show("Translation failed. Error: \r\n" + ex.Message);
+            }
+        }
+
+        public class TranslationResponse
+        {
+            [JsonProperty("responseStatus")]
+            public int ResponseStatus { get; set; }
+
+            [JsonProperty("responseData")]
+            public TranslationData ResponseData { get; set; }
+
+            public string TranslatedText => ResponseData?.TranslatedText;
+        }
+
+        public class TranslationData
+        {
+            [JsonProperty("translatedText")]
+            public string TranslatedText { get; set; }
+        }
+
+        public class LanguageDetectionResponse
+        {
+            [JsonProperty("data")]
+            public DetectionData Data { get; set; }
+        }
+
+        public class DetectionData
+        {
+            [JsonProperty("detections")]
+            public List<Detection> Detections { get; set; }
+        }
+
+        public class Detection
+        {
+            [JsonProperty("language")]
+            public string Language { get; set; }
+
+            [JsonProperty("isReliable")]
+            public bool IsReliable { get; set; }
+
+            [JsonProperty("confidence")]
+            public float Confidence { get; set; }
+        }
+
+        private void pictureBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            SwapLanguages();
+        }
+
+        private void SwapLanguages()
+        {
+            var temp = firstLanguage.selectedIndex;
+            firstLanguage.selectedIndex = secondLanguage.selectedIndex;
+            secondLanguage.selectedIndex = temp;
+
+            var tempText = textBox1.Text;
+            textBox1.Text = textBox2.Text;
+            textBox2.Text = tempText;
         }
     }
 }
